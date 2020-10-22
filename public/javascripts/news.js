17e9d2c41723190da3fbc66984daa175c226ec9f
Anouar Ali <anouar.ali@tu-braunschweig.de>
2020-10-22 05:34:39 +0200

    small updates + logo

---
+++ public/javascripts/news.js
@@ -0,0 +1,7 @@
+let news = document.getElementById("newss");
+news.innerText = "bla bla";
+console.log(mygame);
+setInterval(() => {
+    // news.innerText+="my nigga";
+    
+}, 50);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+let info=[" some phrase  " , " i can do anything i want ", "i can be whoever i want"];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express',game:info});
 });
 
 module.exports = router;
